f0cdf660fbed9a29e891a7f0a0dbdc3a197a6fb5
LiquidLemon <LiquidLemon@users.noreply.github.com>
2016-06-30 17:31:35 +0200

    Remove debug string

--- plugins/ctf/fetcher.rb
+++ plugins/ctf/fetcher.rb
@@ -20,7 +20,6 @@ module CTF
       finish = (Time.now + @offset).strftime('%s')
       limit = 100
       uri = URI("https://ctftime.org/api/v1/events/?limit=#{limit}&start=#{start}&finish=#{finish}")
-      puts uri
       response = Net::HTTP.get(uri)
       data = JSON.parse(response)
       data.select! { |ctf| !ctf["onsite"] }
