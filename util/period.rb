d4fdb6a963cbeb74388d9d157b86c2ab01b24d87
LiquidLemon <LiquidLemon@users.noreply.github.com>
2016-07-28 14:07:01 +0200

    Add Period#from_seconds
    The `#from_seconds` method allows to create a `Period` object from
    a known time amount expressed in seconds.

--- util/period.rb
+++ util/period.rb
@@ -1,41 +1,55 @@
 class Period
 
-  def initialize(weeks: 0, days: 0, hours: 0)
-    @periods = {}
-    @periods[:weeks] = weeks
-    @periods[:days] = days
-    @periods[:hours] = hours
+  UNIT_MULTIPLIERS = {
+    :weeks => 604800,
+    :days => 86400,
+    :hours => 3600,
+    :minutes => 60,
+    :seconds => 1
+  }
+
+  def initialize(units)
+    @units = units
+  end
+
+  def self.from_seconds(seconds)
+    units = {}
+    UNIT_MULTIPLIERS.each_pair do |unit, multiplier|
+      unless seconds < multiplier
+        units[unit] = seconds / multiplier
+        seconds %= multiplier
+      end
+    end
+    return self.new(units)
   end
 
   def to_seconds
-    secs = @periods[:weeks] * 7 * 24 * 60 * 60
-    secs += @periods[:days] * 24 * 60 * 60
-    secs += @periods[:hours] * 60 * 60
-    return secs
+    @units.reduce(0) do |acc, (key, val)|
+      acc += val * UNIT_MULTIPLIERS[key]
+    end
   end
   alias_method :to_i, :to_seconds
 
-  def to_s
-    descriptions = []
-    @periods.each_pair do |k, v|
-      unless v == 0
-        str = k.to_s
-        str = str[0...-1] if v == 1
-        descriptions << "#{v} #{str}"
+  def to_s(accuracy = :s)
+    descriptions = @units.reduce([]) do |acc, (desc, val)|
+      unless val == 0
+        str = desc.to_s
+        str = str[0...-1] if val == 1
+        acc << "#{val} #{str}"
       end
     end
-    str = ''
-    descriptions.each_with_index do |desc, i|
+
+    descriptions.each_with_index.reduce('') do |acc, (desc, i)|
       left = descriptions.length - i
       case left
       when 1
-        str << desc
+        acc << desc
       when 2
-        str << "#{desc} and "
+        acc << "#{desc} and "
       else
-        str << "#{desc}, "
+        acc << "#{desc}, "
       end
     end
-    return str
+
   end
 end
