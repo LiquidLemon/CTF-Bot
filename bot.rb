0eceea3bf18fe2c12dc2b977e4427ba2dfe326b2
LiquidLemon <LiquidLemon@users.noreply.github.com>
2016-08-11 16:46:49 +0200

    Move the config to a separate file

--- bot.rb
+++ bot.rb
@@ -3,28 +3,22 @@ require_relative 'plugins/ctf'
 require_relative 'plugins/quit'
 require_relative 'plugins/version'
 require_relative 'util/period'
-
-authorized = ['LiquidLemon']
+require_relative 'config'
 
 bot = Cinch::Bot.new do
   configure do |c|
-    c.server = 'irc.hackthissite.org'
-    c.channels = ['#liquidbot']
-    c.nick = 'CTF-Bot'
+    c.server = CONFIG[:server]
+    c.channels = CONFIG[:channels]
+    c.nick = CONFIG[:nick]
     c.user = 'CTF-Bot'
     c.plugins.plugins = [CTFPlugin, QuitPlugin, VersionPlugin]
     c.plugins.options[CTFPlugin] = { 
-      lookahead: Period.new(weeks: 4),
-      mark_highschool: true,
-      announce_periods: [
-          Period.new(weeks: 2),
-          Period.new(weeks: 1),
-          Period.new(days: 1),
-          Period.new(hours: 1)
-      ]
+      lookahead: CONFIG[:lookahead],
+      mark_highschool: CONFIG[:mark_highschool],
+      announce_periods: CONFIG[:announcement_periods]
     }
     c.plugins.options[QuitPlugin] = {
-      authorized: authorized,
+      authorized: CONFIG[:admins],
       message: 'Leaving...'
     }
     c.plugins.options[VersionPlugin] = { version: 'CTF-Bot v0.1' }
---
+++ config.rb
@@ -0,0 +1,31 @@
+require_relative 'util/period'
+
+CONFIG = {
+    ## General config
+    server: 'irc.hackthissite.org',
+    channels: ['#ctf'],
+    nick: 'CTF-Bot',
+
+    ## Used for the `!quit` command
+    admins: ['LiquidLemon'],
+
+    ## Specific plugin configuration
+    # The next two options use a helper class `Period`
+    # It takes a hash of units as it's constructor argument
+    # e.g.: Period.new(weeks: 4, days: 2, hours: 1, minutes: 0, seconds: 0)
+    # Null (or zero) values can be omitted
+
+    # Specifies how distant in future the events can be
+    lookahead: Period.new(weeks: 4),
+
+    # Specifies at what time before an event a notice should be made
+    announcement_periods: [
+        Period.new(weeks: 2),
+        Period.new(weeks: 1),
+        Period.new(days: 1),
+        Period.new(hours: 1)
+    ],
+
+    # Whether or not to add a helpful `[HS]` tag in front of high school level events
+    mark_highschool: true
+}
